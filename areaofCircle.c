8758871d48f83a3e3253bde9162451918cc13e58
Harita Agarwal <134896049+hpa179@users.noreply.github.com>
2023-08-11 16:07:58 -0400

    Area

---
+++ areaofCircle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+float area,r;
+printf("Enter the radius of circle : ");
+scanf("%f",&r);
+area=3.14*r*r;
+printf("Area of circle is %f",area);
+return 0;
+}
---
+++ areaofTriangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+float area,b,h;
+printf("Enter the values : ");
+scanf("%f %f",&b,&h);
+area=b*h/2;
+printf("Area of triangle is %f",area);
+return 0;
+}
